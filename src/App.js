d768b8f4359accffe60d0d022e59fcbcdbdf8a31
benayaram <benayaramcreations@gmail.com>
2024-09-15 08:49:54 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import CountryDetails from "./components/CountryDetails";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-
+import "./App.css";  // Ensure App.css is linked
+import LOGO from "./logo.png"
 const App = () => {
   const [countries, setCountries] = useState([]);
 
@@ -22,17 +22,35 @@ const App = () => {
 
   return (
     <Router>
+       <div className="top-right-image-container">
+          <img
+            src={LOGO} // Replace with your image path
+            alt="Top right corner"
+            className="top-right-image"
+          />
+        </div>
+
       <div className="app-container">
         <header className="header">
           <h3>Fast Finder Search Bar</h3>
         </header>
+
+        {/* Image in the top-right corner */}
+       
         <SearchBar countries={countries} />
         <Routes>
           <Route
             path="/"
-            element={<div style={{ padding: "20px" }}>Start typing to search for a country...</div>}
+            element={
+              <div style={{ padding: "20px" }}>
+                Start typing to search for a country...
+              </div>
+            }
           />
-          <Route path="/country/:name" element={<CountryDetails countries={countries} />} />
+          <Route
+            path="/country/:name"
+            element={<CountryDetails countries={countries} />}
+          />
         </Routes>
       </div>
     </Router>
